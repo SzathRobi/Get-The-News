7d4d146e021bc4e8e76aa789b35e3be696c66329
DevVatu <devvatu@gmail.com>
2020-08-18 15:20:48 +0200

    Updates

---
+++ precache-manifest.95d11dbfa8351af01ec65f7508875fc5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6833209ad6991116cffe607d4fe8274b",
+    "url": "/Get-The-News/index.html"
+  },
+  {
+    "revision": "f031621481b0c1bd3ee0",
+    "url": "/Get-The-News/static/css/main.2d66c6a8.chunk.css"
+  },
+  {
+    "revision": "8cc3c95cb14b35940013",
+    "url": "/Get-The-News/static/js/2.ad9fbab2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Get-The-News/static/js/2.ad9fbab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f031621481b0c1bd3ee0",
+    "url": "/Get-The-News/static/js/main.267ac6c1.chunk.js"
+  },
+  {
+    "revision": "c032ac422908649a11fe",
+    "url": "/Get-The-News/static/js/runtime-main.7e5ea172.js"
+  },
+  {
+    "revision": "5061d59ad8a7c81266c5c6b062b596e6",
+    "url": "/Get-The-News/static/media/americas.5061d59a.jpg"
+  },
+  {
+    "revision": "927c58db86b9a6504f1690abbf456d42",
+    "url": "/Get-The-News/static/media/arts.927c58db.jpg"
+  },
+  {
+    "revision": "f4148402bf62a75e45fba7eaf287a3da",
+    "url": "/Get-The-News/static/media/asia.f4148402.jpg"
+  },
+  {
+    "revision": "ec6ac32f4e773e23a72767b0893b9215",
+    "url": "/Get-The-News/static/media/atHome.ec6ac32f.jpg"
+  },
+  {
+    "revision": "92aadc1a4759bd3425453aa4e8f06fcf",
+    "url": "/Get-The-News/static/media/books.92aadc1a.jpg"
+  },
+  {
+    "revision": "28af587d97a79319b5b0547a91db42e2",
+    "url": "/Get-The-News/static/media/business.28af587d.jpg"
+  },
+  {
+    "revision": "a94cb7ed12b303b6d7777559d26a12e9",
+    "url": "/Get-The-News/static/media/climate.a94cb7ed.jpg"
+  },
+  {
+    "revision": "3dc923463bd37d087e56bf8a519f1463",
+    "url": "/Get-The-News/static/media/europe.3dc92346.jpg"
+  },
+  {
+    "revision": "2e22204c903546858ef057d40f9973ec",
+    "url": "/Get-The-News/static/media/food.2e22204c.jpg"
+  },
+  {
+    "revision": "0ab8efd070ba4d03b8f9b84a46680b01",
+    "url": "/Get-The-News/static/media/health.0ab8efd0.jpg"
+  },
+  {
+    "revision": "fe4669197047e8eee7a403b0cba5cb5f",
+    "url": "/Get-The-News/static/media/movies.fe466919.jpg"
+  },
+  {
+    "revision": "58212f7db266703f9fc2100e3f0a9731",
+    "url": "/Get-The-News/static/media/newYork.58212f7d.jpg"
+  },
+  {
+    "revision": "e9c60e44735ecaa16e9780999f27d776",
+    "url": "/Get-The-News/static/media/opinion.e9c60e44.jpg"
+  },
+  {
+    "revision": "115ac3d939522a74217ff934a986e4ba",
+    "url": "/Get-The-News/static/media/parenting.115ac3d9.jpg"
+  },
+  {
+    "revision": "1f9be088195c0e94e368686da94578d2",
+    "url": "/Get-The-News/static/media/podcast.1f9be088.jpg"
+  },
+  {
+    "revision": "02e4d928f4dbcc5f12fe96a4e480087a",
+    "url": "/Get-The-News/static/media/realEstate.02e4d928.jpg"
+  },
+  {
+    "revision": "3c73252375616bee807c7c14917a51d1",
+    "url": "/Get-The-News/static/media/science.3c732523.jpg"
+  },
+  {
+    "revision": "4e32efe9b998445381d97bdbcb5aec11",
+    "url": "/Get-The-News/static/media/sports.4e32efe9.jpg"
+  },
+  {
+    "revision": "ac38c2c6bd7eaf846924647d67bb9ce5",
+    "url": "/Get-The-News/static/media/style.ac38c2c6.jpg"
+  },
+  {
+    "revision": "1f1e10d69732f8b8797558a25f69a02c",
+    "url": "/Get-The-News/static/media/travel.1f1e10d6.jpg"
+  },
+  {
+    "revision": "b266aecef1d345ada55c96ec5ec21f30",
+    "url": "/Get-The-News/static/media/upshot.b266aece.jpg"
+  },
+  {
+    "revision": "02d8cc37697c0c98b22e4270f1640e23",
+    "url": "/Get-The-News/static/media/us.02d8cc37.jpg"
+  },
+  {
+    "revision": "c4fb29d66832b88d7cf3adddc3fa827f",
+    "url": "/Get-The-News/static/media/well.c4fb29d6.jpg"
+  },
+  {
+    "revision": "123b4d0c70c1445cbe6d434b27d0c672",
+    "url": "/Get-The-News/static/media/world.123b4d0c.jpg"
+  }
+]);
